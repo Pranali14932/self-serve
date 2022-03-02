9606a67bd8628f33aff3692b56f35853de35e2d4
Pranali14932 <92152803+Pranali14932@users.noreply.github.com>
2022-03-02 13:59:48 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,167 @@
+$(document).ready(function() {
+    $("#getStarted").click(function() {
+        $("#getStarted").hide();
+        $(".div1").fadeIn();
+        $(".div2").hide();
+        $(".div3").hide();
+        $(".div4").hide();
+        $(".div5").hide();
+    });
+
+    $(".td1").click(function() {
+        $("#getStarted").hide();
+        $(".div1").hide();
+        $(".div2").fadeIn();
+        $(".div3").hide();
+        $(".div4").hide();
+        $(".div5").hide();
+    });
+
+    $(".td2").click(function() {
+        $("#getStarted").hide();
+        $(".div1").hide();
+        $(".div2").hide();
+        $(".div3").fadeIn();
+        $(".div4").hide();
+        $(".div5").hide();
+    });
+
+    $(".td3").click(function() {
+        $("#getStarted").hide();
+        $(".div1").hide();
+        $(".div2").hide();
+        $(".div3").hide();
+        $(".div4").fadeIn();
+        $(".div5").hide();
+    });
+
+    $(".td4").click(function() {
+        $("#getStarted").hide();
+        $(".div1").hide();
+        $(".div2").hide();
+        $(".div3").hide();
+        $(".div4").hide();
+        $(".div5").fadeIn();
+    });
+
+    $(".td5").click(function() {
+        window.location.href = "contact_form.html";
+    })
+});
+
+function previous1() {
+    $(document).ready(function() {
+        $(".previous1").click(function() {
+            $("#getStarted").fadeIn();
+            $(".div1").hide();
+            $(".div2").hide();
+            $(".div3").hide();
+            $(".div4").hide();
+            $(".div5").hide();
+        });
+    });
+}
+
+function previous2() {
+    $(document).ready(function() {
+        $(".previous2").click(function() {
+            $("#getStarted").hide();
+            $(".div1").fadeIn();
+            $(".div2").hide();
+            $(".div3").hide();
+            $(".div4").hide();
+            $(".div5").hide();
+        });
+    });
+}
+
+function previous3() {
+    $(document).ready(function() {
+        $(".previous3").click(function() {
+            $("#getStarted").hide();
+            $(".div1").hide();
+            $(".div2").fadeIn();
+            $(".div3").hide();
+            $(".div4").hide();
+            $(".div5").hide();
+        });
+    });
+}
+
+function previous4() {
+    $(document).ready(function() {
+        $(".previous4").click(function() {
+            $("#getStarted").hide();
+            $(".div1").hide();
+            $(".div2").hide();
+            $(".div3").fadeIn();
+            $(".div4").hide();
+            $(".div5").hide();
+        });
+    });
+}
+
+function previous5() {
+    $(document).ready(function() {
+        $(".previous5").click(function() {
+            $("#getStarted").hide();
+            $(".div1").hide();
+            $(".div2").hide();
+            $(".div3").hide();
+            $(".div4").fadeIn();
+            $(".div5").hide();
+        });
+    });
+}
+
+function next1() {
+    $(document).ready(function() {
+        $(".next1").click(function() {
+            $("#getStarted").hide();
+            $(".div1").hide();
+            $(".div2").fadeIn();
+            $(".div3").hide();
+            $(".div4").hide();
+            $(".div5").hide();
+        });
+    });
+}
+
+function next2() {
+    $(document).ready(function() {
+        $(".next2").click(function() {
+            $("#getStarted").hide();
+            $(".div1").hide();
+            $(".div2").hide();
+            $(".div3").fadeIn();
+            $(".div4").hide();
+            $(".div5").hide();
+        });
+    });
+}
+
+function next3() {
+    $(document).ready(function() {
+        $(".next3").click(function() {
+            $("#getStarted").hide();
+            $(".div1").hide();
+            $(".div2").hide();
+            $(".div3").hide();
+            $(".div4").fadeIn();
+            $(".div5").hide();
+        });
+    });
+}
+
+function next4() {
+    $(document).ready(function() {
+        $(".next4").click(function() {
+            $("#getStarted").hide();
+            $(".div1").hide();
+            $(".div2").hide();
+            $(".div3").hide();
+            $(".div4").hide();
+            $(".div5").fadeIn();
+        });
+    });
+}
